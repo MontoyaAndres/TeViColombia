373b452f9a020e67c0a3f25e7d943259ec9796ce
Andrés Montoya <andresmontoyfcb@gmail.com>
2018-09-07 14:37:37 -0500

    Basic PWA config

--- client/src/server.js
+++ client/src/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const next = require("next");
+const { join } = require("path");
+const { parse } = require("url");
 
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev, dir: "src" });
@@ -10,7 +12,14 @@ app
 	.then(() => {
 		const server = express();
 
-		server.get("*", (req, res) => handle(req, res));
+		server
+			.get("/service-worker.js", (req, res) => {
+				const parsedUrl = parse(req.url, true);
+				const { pathname } = parsedUrl;
+				const filePath = join(__dirname, ".next", pathname);
+				app.serveStatic(req, res, filePath);
+			})
+			.get("*", (req, res) => handle(req, res));
 
 		server.listen(process.env.PORT || 3000, err => {
 			if (err) throw err;
