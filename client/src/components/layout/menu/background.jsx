bf9f5656ba6d280e677c64335d74bf7af78d7520
Andrés Montoya <andresmontoyfcb@gmail.com>
2018-12-03 22:22:03 -0500

    Responsive title and input

--- client/src/components/layout/menu/background.jsx
+++ client/src/components/layout/menu/background.jsx
@@ -1,32 +1,65 @@
-import React from "react";
+import React, { PureComponent } from "react";
 
-const background = () => (
-  <div className="hero-body">
-    <div className="container is-widescreen has-text-centered">
-      <h2
-        className="subtitle is-3 animated pulse"
-        style={{ color: "white", fontWeight: "bold" }}
-      >
-        Tejidos virtuales para el Emprendimiento, las Prácticas profesionales y
-        la Empleabilidad
-      </h2>
-      <div
-        className="field has-addons animated pulse"
-        style={{ padding: "2em" }}
-      >
-        <div className="control is-expanded">
-          <input
-            className="input is-medium is-hovered"
-            type="search"
-            placeholder="Busca lo que tú necesitas"
-          />
-        </div>
-        <div className="control">
-          <a className="button is-primary is-medium">Buscar</a>
+class Background extends PureComponent {
+  state = {
+    width: 0
+  };
+
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener("resize", this.updateWindowDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions = () => {
+    this.setState({ width: window.innerWidth });
+  };
+
+  render() {
+    const { width } = this.state;
+
+    return (
+      <div className="hero-body">
+        <div className="container is-widescreen has-text-centered">
+          <p
+            className="animated pulse"
+            style={{
+              color: "white",
+              fontWeight: "bold",
+              fontSize: width < 600 ? 20 : 40
+            }}
+          >
+            Tejidos virtuales para el Emprendimiento, las Prácticas
+            profesionales y la Empleabilidad
+          </p>
+          <div
+            className="field has-addons animated pulse"
+            style={{ padding: "2em" }}
+          >
+            <div className="control is-expanded">
+              <input
+                className={`input is-hovered ${width < 600 ? "" : "is-medium"}`}
+                type="search"
+                placeholder="Busca lo que tú necesitas"
+              />
+            </div>
+            <div className="control">
+              <a
+                className={`button is-primary ${
+                  width < 600 ? "" : "is-medium"
+                }`}
+              >
+                Buscar
+              </a>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
-export default background;
+export default Background;
